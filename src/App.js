fe5719f8eb5e4463ab52a8c4234a3384986fbb35
Do Quoc Viet <doqv@elcaNet.local>
2022-05-17 11:13:01 +0700

    new branch

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
 import {useState} from 'react';
-
-const gifts = [
-  'CPU i9',
-  'RAM 32GB',
-  'RGB Keyboard'
-]
-
-// API
-const courses = [
-  {
-    id: 1,
-    name: 'HTML, CSS'
-  },
-  {
-    id: 2,
-    name: 'Javascript'
-  },
-  {
-    id: 3,
-    name: 'ReactJS'
-  }
-]
+import Content from './Content';
 
 function App() {
 
-  // '??' must be undefined or null => get after value
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(JSON.parse(localStorage.getItem('jobs')) ?? [])
-
-  const handleSubmit = () => {
-    setJobs(prevState => {
-      const newJobs = [...prevState, job]
-
-      const jsonJobs = JSON.stringify(newJobs)
-      localStorage.setItem('jobs', jsonJobs)
-
-      return newJobs
-    })
-    setJob('')
-
-
-  }
+  const [show, setShow] = useState(false)
 
   return (
     <div className="App">
-      <input
-        value={job}
-        onChange={e => setJob(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Add</button>
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content/>}
     </div>
   );
 }
