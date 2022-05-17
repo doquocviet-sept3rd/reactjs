de2d39c83e3dde2b1b3bff99220a9d5341cc8880
Do Quoc Viet <doqv@elcaNet.local>
2022-05-17 11:00:12 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+const gifts = [
+  'CPU i9',
+  'RAM 32GB',
+  'RGB Keyboard'
+]
+
+// API
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'Javascript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
 
 function App() {
+
+  // '??' must be undefined or null => get after value
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(JSON.parse(localStorage.getItem('jobs')) ?? [])
+
+  const handleSubmit = () => {
+    setJobs(prevState => {
+      const newJobs = [...prevState, job]
+
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJobs
+    })
+    setJob('')
+
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
